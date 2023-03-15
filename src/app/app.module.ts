d980b00d1db8ed55dfb003f17b102c7472a71694
William Stephanus <whal0005@student.monash.edu>
2023-03-15 11:45:10 +0000

    Revert "Beginning to add login page + use angular js"
    This reverts commit e6c42fac6933d1104d763c1d28e26fe5ddc7e11f

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'; 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    ToolbarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
-    ReactiveFormsModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
