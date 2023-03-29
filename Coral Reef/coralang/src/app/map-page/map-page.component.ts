5e90c96399b678fce4fb38bfbdc7f6e5660a700f
William S <william.stephanus.pisb@gmail.com>
2023-03-29 20:50:45 +1100

    Added about animations and map markers

--- Coral Reef/coralang/src/app/map-page/map-page.component.ts
+++ Coral Reef/coralang/src/app/map-page/map-page.component.ts
@@ -9,6 +9,7 @@ import { environment } from './environment';
 })
 export class MapPageComponent {
   map!: mapboxgl.Map;
+  markers: any[] = [];
 
   ngOnInit(): void {
   }
@@ -23,6 +24,12 @@ export class MapPageComponent {
       zoom: 12
     });
     this.map.addControl(new mapboxgl.NavigationControl());
+    const marker1 = new mapboxgl.Marker()
+    .setLngLat([-74.5, 40.5])
+    .setPopup(new mapboxgl.Popup().setHTML('<h3>Example Marker 1</h3><p>Description for marker 1.</p>'))
+    .addTo(this.map);
+
+  this.markers.push(marker1);
   }
   
 }
