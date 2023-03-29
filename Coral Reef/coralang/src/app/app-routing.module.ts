d94e3ed3c32e5c63aa8573c151f96b228ec5c5d5
William S <william.stephanus.pisb@gmail.com>
2023-03-29 19:40:16 +1100

    Added Account and Sign Up Component

--- Coral Reef/coralang/src/app/app-routing.module.ts
+++ Coral Reef/coralang/src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { MapPageComponent } from './map-page/map-page.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 const routes: Routes = [{path: "login", component: LoginPageComponent},
 {path: "map", component: MapPageComponent},
 {path: "about", component: AboutComponent},
+{path: "signup", component: SignupComponent},
 {path: "", component: DashboardComponent},
 ];
 
