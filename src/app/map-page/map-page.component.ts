3d9f717b71a3e9cfe04414c9661651f3f17e1947
William Stephanus <whal0005@student.monash.edu>
2023-03-30 09:59:54 +0000

    Revert "Added about animations and map markers"
    This reverts commit 5e90c96399b678fce4fb38bfbdc7f6e5660a700f

--- src/app/map-page/map-page.component.ts
+++ src/app/map-page/map-page.component.ts
@@ -9,7 +9,6 @@ import { environment } from './environment';
 })
 export class MapPageComponent {
   map!: mapboxgl.Map;
-  markers: any[] = [];
 
   ngOnInit(): void {
   }
@@ -24,12 +23,6 @@ export class MapPageComponent {
       zoom: 12
     });
     this.map.addControl(new mapboxgl.NavigationControl());
-    const marker1 = new mapboxgl.Marker()
-    .setLngLat([-74.5, 40.5])
-    .setPopup(new mapboxgl.Popup().setHTML('<h3>Example Marker 1</h3><p>Description for marker 1.</p>'))
-    .addTo(this.map);
-
-  this.markers.push(marker1);
   }
   
 }
