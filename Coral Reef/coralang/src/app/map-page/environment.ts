fa05a523ec3c3af3fddd3cb9c26b2007485e031a
Jotham Gates <jgat0002@student.monash.edu>
2023-04-04 22:44:58 +1000

    Removed old Coral Reef directory and files

--- Coral Reef/coralang/src/app/map-page/environment.ts
+++
@@ -1,3 +0,0 @@
-import { accessToken } from "mapbox-gl"
-
-export const environment = {mapbox: {accessToken: 'pk.eyJ1Ijoid2lsbGlhbWRvdG8iLCJhIjoiY2xmaXd1bGg5MDlidTN3bzZ6eGF6eTBncyJ9.5OnA6sEVKbuAEGT3tD--Ww'}}
