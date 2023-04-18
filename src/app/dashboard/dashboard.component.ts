ecc03c935939386593b8c62b73961ce25f03e617
Yufeng Zhu <yzhu0091@student.monash.edu>
2023-04-18 20:40:44 +1000

    Add the initial design of the dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
-})
-export class DashboardComponent {
 
+})
+export class DashboardComponent implements OnInit{
+  imageUrls = []
+  imageUrl = this.imageUrls[0]
+  currentIndex = 0
+
+  changeImage(){
+    if (this.currentIndex > this.imageUrls.length)    this.currentIndex = 0
+    this.currentIndex = (this.currentIndex + 1) % this.imageUrls.length
+    this.imageUrl = this.imageUrls[this.currentIndex];
+  }
+
+  ngOnInit() {
+    setInterval(() => {
+      this.changeImage();
+    }, 2000)
+  }
 }
